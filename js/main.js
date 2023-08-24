cb7f81a3c71a33be9ffac358def723b8c68a3770
Shehzab <106922099+shehzab@users.noreply.github.com>
2023-08-24 13:49:51 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function ($) {
+    "use strict";
+    
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+    
+   
+    
+    
+    
+    
+})(jQuery);
+
